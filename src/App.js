b9d613b1540a6e08d4100b84bbcea01dc2e65b6c
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-11-07 12:46:18 -0500

    initial setup done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './pages/Shared/Header/Header';
+import Login from './pages/Login/Login';
+import { RouterProvider } from 'react-router-dom';
+// import routes from './Router/Routes/Routes';
+import router from './Router/Routes/Routes';
+
+function App() {
+  return (
+  <div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+  </div>
+  );
+}
+
+export default App;
